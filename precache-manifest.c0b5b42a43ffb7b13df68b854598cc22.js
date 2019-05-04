05c8350649a6fde7f335ed2727544dd77da004b9
Ronny Legones <legonesronny@gmail.com>
2019-05-03 21:55:12 -0400

    Updates

--- precache-manifest.c0b5b42a43ffb7b13df68b854598cc22.js
+++ precache-manifest.c0b5b42a43ffb7b13df68b854598cc22.js
@@ -1,18 +1,18 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "614311e2001874e95079df0402bc98cf",
+    "revision": "27121b11c8568fd6e0ea288538cb9dba",
     "url": "/the-pasta-recipes/index.html"
   },
   {
-    "revision": "073517ef8ec031f0968a",
-    "url": "/the-pasta-recipes/static/css/main.278968a1.chunk.css"
+    "revision": "41c62a68ee72edb0ef09",
+    "url": "/the-pasta-recipes/static/css/main.7cb43590.chunk.css"
   },
   {
     "revision": "fa303674d48da5317888",
     "url": "/the-pasta-recipes/static/js/2.3e5eadc2.chunk.js"
   },
   {
-    "revision": "073517ef8ec031f0968a",
+    "revision": "41c62a68ee72edb0ef09",
     "url": "/the-pasta-recipes/static/js/main.136057fe.chunk.js"
   },
   {
